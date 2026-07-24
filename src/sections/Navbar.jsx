7d37372456837544f2f92d9200370cf6869d6a20
Tarunna Sen <tarunnasen@gmail.com>
2026-07-24 11:59:06 +0600

    Workiing Navbar on scroll

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,9 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavIcon from "../components/NavIcon";
 import { Home, User, Info, Award, Settings, FolderDot } from "lucide-react";
 
 export default function Navbar() {
+  const sections = [
+    { id: "home", name: "Home" },
+    { id: "about", name: "About" },
+    { id: "experience", name: "Experience" },
+    { id: "projects", name: "Projects" },
+    { id: "skills", name: "Skills" },
+    { id: "achievements", name: "Achievements" },
+  ];
   const [curr, setCurr] = useState("Home");
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const section = sections.find((s) => s.id === entry.target.id);
+
+            if (section) {
+              setCurr(section.name);
+            }
+          }
+        });
+      },
+      {
+        threshold: 0.2,
+      },
+    );
+
+    sections.forEach((section) => {
+      const el = document.getElementById(section.id);
+      if (el) observer.observe(el);
+    });
+
+    return () => observer.disconnect();
+  }, []);
 
   return (
     <nav className="sticky top-4 z-50 flex justify-center px-4">
@@ -47,7 +81,7 @@ export default function Navbar() {
           curr={curr}
           setCurr={setCurr}
           text="Achievements"
-          href="#Achievements"
+          href="#achievements"
           icon={<Award size={18} />}
         />
       </div>
